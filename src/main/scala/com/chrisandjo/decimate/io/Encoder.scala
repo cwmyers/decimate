5a51950095a1938c5e10fc7e71eda809661178f1
bskyb-myersch <christopher.myers@bskyb.com>
2013-05-20 16:58:55 +0100

    I think I've figured out monad transformers.

--- src/main/scala/com/chrisandjo/decimate/io/Encoder.scala
+++ src/main/scala/com/chrisandjo/decimate/io/Encoder.scala
@@ -1,5 +1,7 @@
 package com.chrisandjo.decimate.io
 
+import scalaz._
+import Scalaz._
 import scalaz.effect.IO
 import java.io._
 import scala.sys.process.Process
@@ -9,39 +11,49 @@ import scala.sys.process.Process
  * User: chris
  * Date: 16/05/2013
  * Time: 21:12
- * Copyright (c) Chris Myers 2010
+ * Copyright (c) Chris Myers 2013
  */
 object Encoder {
 
-  def findFfmpeg: IO[Option[String]] = IO {
-    List("/usr/local/bin/ffmpeg", "/usr/bin/ffmpeg").find {
-      new File(_).exists
+  def findFfmpeg: OptionT[IO, String] = {
+    OptionT[IO, String](IO {
+      List("/usr/local/bin/ffmpeg", "/usr/bin/ffmpeg").find {
+        new File(_).exists
+      }
     }
+    )
   }
 
-  def callFfmpeg(videoFile: String, ffmpegBin: String): IO[Stream[String]] = IO {
-    val command = s"$ffmpegBin $videoFile  '-vcodec libx264 -b:v 2000k -refs 3 -keyint_min 29 -x264opts trellis=0 -acodec libfaac -ar 44100 -ab 160k -s 1024x576' /tmp/out.mp4"
+  def getCommand(ffmpegBin: String, videoFile:String)= s"$ffmpegBin $videoFile  '-vcodec libx264 -b:v 2000k -refs 3 -keyint_min 29 -x264opts trellis=0 -acodec libfaac -ar 44100 -ab 160k -s 1024x576' /tmp/out.mp4"
+
+  def callFfmpeg(command: String): IO[Stream[String]] = IO {
     Process(command).lines_!
   }
 
-  def getStream(fileName: String):IO[Stream[String]] = for {
-    ffmpegOption <- findFfmpeg
-    ffmpegBin <- ffmpegOption
-    stream <- callFfmpeg(fileName, ffmpegBin)
+  def getStream(fileName: String): OptionT[IO,Stream[String]] = {
+    val a:OptionT[IO,String] = findFfmpeg map {getCommand(_, fileName)}
+    a map {Process(_).lines_!}
+  }
 
-  } yield stream
+  //  = for {
+  //    ffmpegBin <- findFfmpeg
+  //    stream <- callFfmpeg(fileName, ffmpegBin)
+  //
+  //  } yield stream
 
 
   def encode(fileName: String): IO[Unit] = {
 
 
-    getStream(fileName) map {
-      a: Stream[String] =>
+    val b = getStream(fileName) map {
+      a  =>
         a map {
           _.length
         } foreach (println)
 
     }
+
+    b.getOrElse(Unit.box {println("nothing")})
 
 
   }
