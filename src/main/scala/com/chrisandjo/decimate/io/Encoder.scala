371a0537e1139ca940cdfbe74298f868d7b66417
Chris Myers <christopher.myers@bskyb.com>
2013-05-20 07:42:36 +0100

    Bug with for comprehension and IO moanad

--- src/main/scala/com/chrisandjo/decimate/io/Encoder.scala
+++ src/main/scala/com/chrisandjo/decimate/io/Encoder.scala
@@ -1,6 +1,7 @@
 package com.chrisandjo.decimate.io
 
 import scalaz.effect.IO
+import java.io._
 import scala.sys.process.Process
 
 /**
@@ -11,18 +12,37 @@ import scala.sys.process.Process
  * Copyright (c) Chris Myers 2010
  */
 object Encoder {
-      def encode(fileName:String):IO[Unit] = {
-        val ffmpegIO  = IO {
-          Process("/usr/local/bin/ffmpeg -y -i "+fileName+" -vcodec libx264 -b:v 2000k -refs 3 -keyint_min 29 -x264opts trellis=0 -acodec libfaac -ar 44100 -ab 160k -s 1024x576 /tmp/out.mp4").lines_!
-        }
+
+  def findFfmpeg: IO[Option[String]] = IO {
+    List("/usr/local/bin/ffmpeg", "/usr/bin/ffmpeg").find {
+      new File(_).exists
+    }
+  }
+
+  def callFfmpeg(videoFile: String, ffmpegBin: String): IO[Stream[String]] = IO {
+    val command = s"$ffmpegBin $videoFile  '-vcodec libx264 -b:v 2000k -refs 3 -keyint_min 29 -x264opts trellis=0 -acodec libfaac -ar 44100 -ab 160k -s 1024x576' /tmp/out.mp4"
+    Process(command).lines_!
+  }
+
+  def getStream(fileName: String):IO[Stream[String]] = for {
+    ffmpegOption <- findFfmpeg
+    ffmpegBin <- ffmpegOption
+    stream <- callFfmpeg(fileName, ffmpegBin)
+
+  } yield stream
 
 
-
-        ffmpegIO map { a : Stream[String] =>
-          a map {_.length} foreach(println)
-
-        }
+  def encode(fileName: String): IO[Unit] = {
 
 
-      }
+    getStream(fileName) map {
+      a: Stream[String] =>
+        a map {
+          _.length
+        } foreach (println)
+
+    }
+
+
+  }
 }
