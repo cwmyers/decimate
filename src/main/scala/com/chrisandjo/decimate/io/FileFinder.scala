9e91e00b23323b39966832561973246ec3c238ce
bskyb-myersch <christopher.myers@bskyb.com>
2013-05-22 09:52:41 +0100

    Added ffmpegWrapper finder

--- src/main/scala/com/chrisandjo/decimate/io/FileFinder.scala
+++ src/main/scala/com/chrisandjo/decimate/io/FileFinder.scala
@@ -1,5 +1,8 @@
 package com.chrisandjo.decimate.io
 
+import scalaz.effect.IO
+import java.io.File
+
 /**
  * Created by IntelliJ IDEA.
  * User: chris
@@ -7,6 +10,10 @@ package com.chrisandjo.decimate.io
  * Time: 20:59
  * Copyright (c) Chris Myers 2010
  */
-class FileFinder {
+object FileFinder {
+  def findFile(path:List[String]): IO[Option[String]] = IO{path.find(new File(_).exists)}
 
+  def findFfmpeg = findFile(List("/usr/local/bin/ffmpeg", "/usr/bin/ffmpeg"))
+
+  def findFfmpegWrapper = findFile(List("/Users/grailsuser/decimate/src/main/scripts/ffmpeg_wrapper.sh"))
 }
