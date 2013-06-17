659d4b71c327b16d9f0f2697abf237b9db641c66
Chris Myers <christopher.myers@bskyb.com>
2013-06-17 21:17:13 +0100

    Thanks to Travis Brown got the Reader working without stair-stepping.

--- src/main/scala/com/chrisandjo/decimate/io/Main.scala
+++ src/main/scala/com/chrisandjo/decimate/io/Main.scala
@@ -12,7 +12,8 @@ object Main {
       ffmpegLocations = List("/usr/local/bin/ffmpeg", "/usr/bin/ffmpeg"),
       ffmpegWrapperLocations = List(
         "/Users/grailsuser/decimate/src/main/scripts/ffmpeg_wrapper.sh",
-        "/Users/chris/Development/decimate/src/main/scripts/ffmpeg_wrapper.sh"))
+        "/Users/chris/Development/decimate/src/main/scripts/ffmpeg_wrapper.sh")
+    )
     Encoder.encode("/Users/chris/Development/skyvideo-encoderbot/test/resources/video/test-master.mov")(config).run.unsafePerformIO()
   }
 }
