d7573d96a516d9cff4c7796f2b57c01d1a6adce7
Chris Myers <christopher.myers@bskyb.com>
2013-09-04 21:54:23 +0100

    Moving to a playframework codebase

--- project/build.scala
+++ project/build.scala
@@ -1,19 +1,49 @@
 import sbt._
 import Keys._
+import play.Project._
 
-object build extends Build {
+object ApplicationBuild extends Build {
 
-  val gcsettings = Defaults.defaultSettings
+  val appName         = "decimate"
+  val appVersion      = "0.1-SNAPSHOT"
+  val appScalaVersion    = "2.10.2"
 
-  val gc = TaskKey[Unit]("gc", "runs garbage collector")
-  val gcTask = gc := {
-    println("requesting garbage collection")
-    System gc()
-  }
-
-  lazy val project = Project (
-    "project",
-    file("."),
-    settings = gcsettings ++ Seq(gcTask)
+  val sharedDependencies = Seq(
+    "org.scalaz" %% "scalaz-core" % "7.0.2",
+    "org.scalaz" %% "scalaz-effect" % "7.0.2",
+    "io.argonaut" %% "argonaut" % "6.0.1"
   )
+
+  val appDependencies = Seq(
+  )
+
+  val transcoderService = Project("transcoder-service", file("modules/encoder"))
+          .settings(scalaVersion := appScalaVersion)
+          .settings(libraryDependencies := sharedDependencies ++ Seq(
+    "play" %% "play" % "2.1.3",
+    "play" %% "play-jdbc" % "2.1.3"
+  ))
+          .settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
+          .settings(
+    resolvers ++= Seq(
+      "typesafe" at "http://repo.typesafe.com/typesafe/repo"
+    )
+  )
+          .settings(publishArtifact in (Test, packageBin) := true)
+
+  val main = play.Project(appName, appVersion, sharedDependencies ++ appDependencies)
+          .settings(defaultScalaSettings: _*)
+          .settings(scalaVersion := appScalaVersion)
+          .settings(
+    javaOptions in Test += "-Dconfig.file=" + "conf/"+ Option(System.getenv("TEST_CONFIG_FILE")).getOrElse("test.conf")
+  )
+          .settings(
+    resolvers ++= Seq(
+      "test argonaut" at "https://oss.sonatype.org/content/repositories/snapshots"
+    )).dependsOn(transcoderService % "test->test;compile->compile")
+          .settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
+          .settings(parallelExecution in Test := false)
+          .settings(testOptions in Test += Tests.Setup( () => println("Setup") ))
+          .settings(testOptions in Test += Tests.Cleanup( () => println("Cleanup") )).aggregate(transcoderService)
+
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,13 +1,12 @@
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.4")
+// Comment to get more information during initialization
+logLevel := Level.Warn
 
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.0")
+// The Typesafe repository 
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+// Use the Play sbt plugin for Play projects
+addSbtPlugin("play" % "sbt-plugin" % "2.1.3")
+
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.4.0")
-
-// addSbtPlugin("io.spray" % "sbt-revolver" % "0.6.2")
-
-// addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.6")
-
-// addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.0")
-
-
